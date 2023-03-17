c1c9f426aafe4bfdee496b9a0fe9be7ee5f88058
Janek Spaderna <janek@spaderna.eu>
2023-03-17 20:07:23 +0100

    Simplify entrypoint

--- freest-type-generator/app/Main.hs
+++ freest-type-generator/app/Main.hs
@@ -24,21 +24,17 @@ argTsize = option auto
            <> metavar "TS"
            <> help "Generate types of size TS" )
 
-argPseed :: Parser Int
-argPseed = option auto
+argPseed :: Parser (Maybe Int)
+argPseed = optional . option auto $
            ( long "pseed"
            <> short 'P'
-           <> value (0-1)
-           -- <> showDefault
            <> metavar "PSEED"
            <> help "Use PSEED to generate protocols" )
 
-argTseed :: Parser Int
-argTseed = option auto
+argTseed :: Parser (Maybe Int)
+argTseed = optional . option auto $
            ( long "tseed"
            <> short 'T'
-           <> value (0-1)
-           -- <> showDefault
            <> metavar "TSEED"
            <> help "Use TSEED to generate protocols" )
 
@@ -47,12 +43,6 @@ argToolbox = switch
           ( long "toolbox"
          <> short 'b'
          <> help "Use predefined toolbox" )
-
-argRepeatable :: Parser Bool
-argRepeatable = switch
-              ( long "repeatable"
-              <> short 'r'
-              <> help "Repeatable run with PSEED and TSEED")
 
 argProtocols :: Parser [String]
 argProtocols = many (argument str (metavar "PROTO..."))
@@ -64,7 +54,6 @@ cmdLineArgs = GenConfig
               <*> argPseed
               <*> argTseed
               <*> argToolbox
-              <*> argRepeatable
               <*> argProtocols
 
 opts :: ParserInfo GenConfig
--- freest-type-generator/src/GenExamples.hs
+++ freest-type-generator/src/GenExamples.hs
@@ -1,96 +1,89 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
 module GenExamples (runGenerator, GenConfig(..)) where
 
+import Data.Either (partitionEithers)
+import Data.Foldable
+import Data.List (intercalate)
+import System.Exit
+import System.IO
 import System.Random
 import Test.QuickCheck
-import Types
-import Generators
-
-import qualified PrettyFreeST as PF
-import qualified PrettyAlgST as PA
-
-import Examples
-
--- for repeatable random number generation
-
 import Test.QuickCheck.Gen (unGen)
 import Test.QuickCheck.Random (mkQCGen)
+
+import Examples
+import Generators
+import PrettyAlgST qualified as PA
+import PrettyFreeST qualified as PF
+import Types
 
 data GenConfig = GenConfig
   { psize :: Int
   , tsize :: Int
-  , pseed :: Int
-  , tseed :: Int
+  , pseed :: Maybe Int
+  , tseed :: Maybe Int
   , toolbox :: Bool
-  , repeatable :: Bool
   , protocols :: [String]
   }
-
 
 protocolEnvironment :: [Protocol]
 protocolEnvironment =  [pIntListP, pListP, pArith, pStream, pSeq, pEither, pRepeat]
 
+toolboxEnvironment :: [Protocol]
+toolboxEnvironment = [pSeq, pEither, pRepeat] 
+
+resolveSeed :: Maybe Int -> IO Int
+resolveSeed = maybe randomIO pure
+
 runGenerator :: GenConfig -> IO ()
-runGenerator config
-  | repeatable config = do
-    newTseed <- if tseed config /= -1
-                then pure (tseed config)
-                else randomIO
-    newPseed <- if pseed config /= -1
-                then pure (pseed config)
-                else randomIO
-    let newconfig = config { tseed = newTseed, pseed = newPseed }
-    runRepeatable newconfig
-  | otherwise =
-    if toolbox config
-    then withToolbox config
-    else randomProtocols config
-
-randomProtocols :: GenConfig -> IO ()
-randomProtocols config = do
-  ps <- generate $ genProtocols (psize config)
-  withProtocols ps config
-
-withProtocols :: [Protocol] -> GenConfig -> IO ()
-withProtocols ps config = do
-  let pnenv  = map (\p -> (prName p, prParameters p)) ps
-      -- pnames = map prName ps
-      -- params = head (map prParameters ps)
-  t <- generate $ genType (tsize config) TL [] pnenv
+runGenerator config = do
+  -- Generate protocols/select protocols.
+  newPSeed <- resolveSeed (pseed config)
+  newTSeed <- resolveSeed (tseed config)
+  putStrLn "-- Rerun with"
+  putStrLn $ "--pseed=" ++ show newPSeed
+  putStrLn $ "--tseed=" ++ show newTSeed
+  putStrLn ("--psize=" ++ show (psize config))
+  putStrLn ("--tsize=" ++ show (tsize config))
+  ps <-
+    if | null (protocols config) && toolbox config ->
+           pure toolboxEnvironment
+       | null (protocols config) ->
+           generateWithSeed newPSeed $ genProtocols (psize config)
+       | toolbox config ->
+           selectProtocols (protocols config) toolboxEnvironment
+       | otherwise ->
+           selectProtocols (protocols config) protocolEnvironment
+  let pnenv = map (\p -> (prName p, prParameters p)) ps
+  -- let pnames = map prName ps
+  --     params = head (map prParameters ps)
+  t <- generateWithSeed newTSeed $ genType (tsize config) TL [] pnenv
   let m = Module ps [t]
   putStrLn "--- protocol and type in AlgST syntax ---"
   PA.putPretty $ PA.prettyModule m
   putStrLn "-----------------------------------------"
   putStrLn "--- corresponding type in FreeST syntax ---"
   PF.putPretty $ PF.prettyModule m
-
-withToolbox :: GenConfig -> IO ()
-withToolbox = withSelectedProtocols ["Seq", "Either", "Repeat"]
-
-withSelectedProtocols :: [Name] -> GenConfig -> IO ()
-withSelectedProtocols pnames =
-  withProtocols [ p | pn <- pnames, Just p <- [lookup pn (map namedProtocol protocolEnvironment)]]
 
 generateWithSeed :: Int -> Gen a -> IO a
 generateWithSeed seed gena =
   do let r = mkQCGen seed
      return (unGen gena r 30)
 
-runRepeatable :: GenConfig -> IO ()
-runRepeatable conf = do
-  putStrLn "--- generating with configuration ---"
-  putStrLn ("--pseed=" ++ show (pseed conf))
-  putStrLn ("--tseed=" ++ show (tseed conf))
-  putStrLn ("--psize=" ++ show (psize conf))
-  putStrLn ("--tsize=" ++ show (tsize conf))
-  ps <- generateWithSeed (pseed conf) $ genProtocols (psize conf)
-  let pnenv  = map (\p -> (prName p, prParameters p)) ps
-  -- let pnames = map prName ps
-  --     params = head (map prParameters ps)
-  t <- generateWithSeed (tseed conf) $ genType (tsize conf) TL [] pnenv
-  let m = Module ps [t]
-  putStrLn "--- protocol and type in AlgST syntax ---"
-  PA.putPretty $ PA.prettyModule m
-  putStrLn "-----------------------------------------"
-  putStrLn "--- corresponding type in FreeST syntax ---"
-  PF.putPretty $ PF.prettyModule m
+selectProtocols :: [String] -> [Protocol] -> IO [Protocol]
+selectProtocols names penv =
+  case partitionEithers (lookupProtocol <$> names) of
+    ([], ps) -> pure ps
+    (notFound, _) -> do
+      for_ notFound \name ->
+        hPutStrLn stderr $ "unknown protocol " ++ name
+      hPutStrLn stderr $ "known protocols: " ++
+        intercalate ", " [ n | Protocol { prName = Name n } <- penv ]
+      exitFailure
+  where
+    lookupProtocol name =
+      maybe (Left name) Right $
+        lookup (Name name) (map namedProtocol penv)
