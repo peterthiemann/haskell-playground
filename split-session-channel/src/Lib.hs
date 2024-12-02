ffb8fa00d984149f6b02a5c9da20fccb6360b91b
Peter Thiemann <peter.thiemann@gmail.com>
2024-12-02 11:50:28 +0100

    made use IORef switchable to MVar

--- split-session-channel/src/Lib.hs
+++ split-session-channel/src/Lib.hs
@@ -2,8 +2,19 @@ module Lib
     ( create, receive, send, split, Lib.drop, close, Session
     ) where
 
-import Control.Concurrent (MVar, readMVar, takeMVar, newEmptyMVar, newMVar, putMVar)
-import Data.IORef
+import Control.Concurrent (MVar, readMVar, takeMVar, newEmptyMVar, newMVar, putMVar, modifyMVar)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+
+
+-- * Unsafe update operation for IORefs
+
+modifyIORef :: IORef a -> (a -> IO (a, b)) -> IO b
+modifyIORef r f = do
+  a0 <- readIORef r
+  (a1, b) <- f a0
+  writeIORef r a1
+  return b
+
 
 -- * API of FreeST style synchronous channels
 
@@ -35,19 +46,25 @@ data SessionState a
   | Dropped
   | Closed
 
+modify :: Session a -> (SessionState a -> IO (SessionState a, b)) -> IO b
+newSession :: MVar (PrimitiveChan a) -> MVar (PrimitiveChan a) -> IO (Session a)
+
 type Session a = IORef (SessionState a)
+modify = modifyIORef
+newSession now next = newIORef $ Active now next
 
+-- type Session a = MVar (SessionState a)
+-- modify = modifyMVar
+-- newSession now next = newMVar $ Active now next
 
-unwrap :: Session a -> String -> (MVar (PrimitiveChan a) -> MVar (PrimitiveChan a) -> IO b) -> IO b
-unwrap c msg action = do
-  cs <- readIORef c
-  case cs of
-    Active now next ->
-      action now next
-    Dropped ->
-      error ("trying to " ++ msg ++ " on dropped channel")
-    Closed ->
-      error ("trying to " ++ msg ++ " on closed channel")
+unwrap :: Session a -> String -> (MVar (PrimitiveChan a) -> MVar (PrimitiveChan a) -> IO (SessionState a, b)) -> IO b
+unwrap s msg action = do
+  modify s go
+    where
+      -- go :: SessionState a -> IO (SessionState a, b)
+      go (Active now next) = action now next                 
+      go Dropped = error ("trying to " ++ msg ++ " on dropped channel")
+      go Closed  = error ("trying to " ++ msg ++ " on closed channel")
 
 -- | Create a new channel represented by two channel endpoints in active state 
 create :: IO (Session a, Session a)
@@ -55,23 +72,25 @@ create = do
   pc <- createPrim
   now1 <- newMVar pc
   next1 <- newEmptyMVar
-  chan1 <- newIORef (Active now1 next1)
+  chan1 <- newSession now1 next1
   now2 <- newMVar pc
   next2 <- newEmptyMVar
-  chan2 <- newIORef (Active now2 next2)
+  chan2 <- newSession now2 next2
   return (chan1, chan2)
 
 receive :: Session a -> IO a
 receive c = 
-  unwrap c "receive" $ \ now _next -> do
+  unwrap c "receive" $ \ now next -> do
     pc <- readMVar now
-    receivePrim pc
+    rv <- receivePrim pc
+    return (Active now next, rv)
 
 send :: a -> Session a -> IO ()
 send a c =
-  unwrap c "send" $ \ now _next -> do
+  unwrap c "send" $ \ now next -> do
     pc <- readMVar now
     sendPrim a pc
+    return (Active now next, ())
 
 -- | Split a channel by returning its borrow.
 --  Splitting suspends the argument channel until it is released by the borrow
@@ -79,9 +98,8 @@ split :: Session a -> IO (Session a)
 split c =
   unwrap c "split" $ \ now next -> do
     link <- newEmptyMVar
-    borrow <- newIORef (Active now link)
-    writeIORef c (Active link next)
-    return borrow
+    borrow <- newSession now link
+    return (Active link next, borrow)
 
 -- | Drop a channel. Usually applied to a borrow to return possession to the original owner.
 drop :: Session a -> IO ()
@@ -89,11 +107,11 @@ drop c =
   unwrap c "drop" $ \ now next -> do
     pc <- takeMVar now
     putMVar next pc
-    writeIORef c Dropped
+    return (Dropped, ())
 
 -- | Close a channel. Not strictly necessary, but may free some resources.
 close :: Session a -> IO ()
 close c =
   unwrap c "close " $ \ now _next -> do
     _ <- takeMVar now
-    writeIORef c Closed
+    return (Closed, ())
