d4d4c4e0788fa272e5707baa5af74096efe12549
Peter Thiemann <peter.thiemann@gmail.com>
2024-12-08 16:50:02 +0100

    more diagnostics

--- split-session-channel/app/Main.hs
+++ split-session-channel/app/Main.hs
@@ -9,7 +9,7 @@ type Sync = MVar ()
 
 main :: IO ()
 main = do
-  flags <- fmap concat $ mapM runner [0 .. 19]
+  flags <- fmap concat $ mapM runner [0 .. 999]
   waitFor flags
 
 waitFor :: [Sync] -> IO ()
@@ -21,6 +21,14 @@ waitFor (m : ms) = do
 forkAndSync :: Sync -> IO a -> IO ThreadId
 forkAndSync flag io =
   forkFinally io (const $ putMVar flag ())
+
+----------------------------------------------------------------------
+
+message :: String -> IO ()
+message _ = return ()
+-- message = putStrLn
+
+----------------------------------------------------------------------
 
 runner :: Int -> IO [Sync]
 runner i = do
@@ -39,15 +47,22 @@ client i x = do
   Lib.send 42 x
   Lib.send 17 x
   r <- Lib.receive x
-  putStrLn (show r)
+  putStrLn ("receive " ++ show r)
+  message ("r-close " ++ show i)
   Lib.close x
 
 server :: Sync -> Int -> Lib.Session Int -> IO ()
 server flag i y = do
   when (i `mod` 2 /= 0) yield
   b <- Lib.split y
-  _tid <- forkAndSync flag $ do { when ((i `mod` 4) `div` 2  == 1) yield; _r42 <- Lib.receive b; Lib.drop b }
+  _tid <- forkAndSync flag $ do
+    when ((i `mod` 4) `div` 2  == 1) yield
+    r42 <- Lib.receive b
+    when (r42 /= 42) $ putStrLn ("wrong value in " ++ show (1000 + i))
+    message ("b-drop " ++ show i)
+    Lib.drop b
   when ((i `mod` 4) `div` 2 == 0) yield
   _r17 <- Lib.receive y
   Lib.send (1000 + i) y
+  putStrLn ("s-close " ++ show (1000 + i))
   Lib.close y
